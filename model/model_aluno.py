310ceb71c23c6bc231daec1f4c57b24111e8031c
Jefferson David <napsterdavid@gmail.com>
2023-08-24 09:22:10 -0300

    configuração do model, com o metodos que criar o aluno

---
+++ model/model_aluno.py
@@ -0,0 +1,19 @@
+import pymysql
+class ModelAluno:
+    def __init__(self):
+        self.conexao = pymysql.connect(
+            host="localhost",
+            user="root",
+            password="@Cursos21",
+            database="serpro",
+            cursorclass = pymysql.cursors.DictCursor
+        )
+    def criarAluno (self, nome, idade, peso):
+        try: 
+         with self.conexao.cursor() as cursor:
+            criandoAluno = "INSERT INTO tb_alunos (nome, idade, peso) VALUES (%s, %s, %s)"
+            valores = (nome, idade, peso)
+            cursor.execute(criandoAluno, valores)
+            self.conexao.commit()
+        finally:
+           self.connection.close()
